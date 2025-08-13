e85fb606ad69f796b8e61aefb1001d423d2ac2ba
punjabiweddingmc <punjabiweddingmc@gmail.com>
2025-08-13 08:38:59 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Punjabi Wedding MC</h1>
+      <p>Wedding MC • Dhol • Host</p>
+      <p>Website coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
